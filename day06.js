9c0b7fa4041aed30e1aee3bbcb1a26e79449ee0e
Kate Lee <tiramisu9156@gmail.com>
2018-08-09 21:35:02 -0400

    first commit

---
+++ day06.js
@@ -0,0 +1,9 @@
+alert('Javascript is cool');
+prompt("Hey! You prompted me!")
+
+console.log("Hey ma! Look I'm typing Javascript")
+console.log(5 + 11)
+
+function change(){
+  document.getElementById('el').innerHTML = "NEW TEXT";
+}
